8bfa90563b56b3ea1eb40ece5746578d3eda0c00
Eric Karp <karpe2@carleton.edu>
2026-04-29 15:24:49 -0500

    Remove Firebase config from firebaseConfig.ts
    Removed sensitive Firebase configuration details.

--- junk-mail/app/utils/firebaseConfig.ts
+++ junk-mail/app/utils/firebaseConfig.ts
@@ -9,13 +9,6 @@ import { getReactNativePersistence } from '@firebase/auth/dist/rn/index.js';
 
 // Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyBOwbEFzAMjKGWRFBekvstSkeyvY_oB_n4",
-  authDomain: "junk-mail-comps.firebaseapp.com",
-  projectId: "junk-mail-comps",
-  storageBucket: "junk-mail-comps.firebasestorage.app",
-  messagingSenderId: "906231894382",
-  appId: "1:906231894382:android:3f5b2c7d2a401653cdb17f",
-  measurementId: "G-P9FH9H7QZK"
 };
 
 
